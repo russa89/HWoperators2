847a74ea518e91f357d54b11d17e162450f5ce52
russa89 <natast2189@gmail.com>
2024-04-03 22:42:52 +0300

    homework is done

--- src/Main.java
+++ src/Main.java
@@ -21,12 +21,49 @@ public class Main {
         }
         System.out.println("\nЗадание 3");
         int year = 2021;
-        if (year >= 1584 && (year%4 == 0 && year%100 != 0) || year%400 == 0) {
+        if (year >= 1584 && (year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println(year + " год является високосным!");
         } else System.out.println(year + " год не является високосным!");
 
         System.out.println("\nЗадание 4");
+        int deliveryDistance = 95;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется 1 день.");
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется 2 дня.");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется 3 дня.");
+        } else
+            System.out.println("Доставка невозможна.");
+
+        System.out.println("\nЗадание 5");
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует.");
+        }
+
+
 
     }
-
 }
